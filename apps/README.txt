6c22ba98e3f4bba8611479da85f8c64d54dc2c63
amichai-bd <amichaibd@gmail.com>
2021-08-17 22:28:16 +0300

    Fix alive test

--- apps/README.txt
+++ apps/README.txt
@@ -1,2 +1,10 @@
-dos2unix gpc_compile_link.sh
+1) Design an assembly test and name it <test>.S
+
+2) run the command  ./ss_rvc_compile_link.sh alive
+
+#to see the compile, link, elf_read, .sv, move to verif dir please read the script:
+cat ./ss_rvc_compile_link.sh
+
+# sometimes this is needed
+dos2unix ss_rvc_compile_link.sh
 
--- apps/ss_rvc_compile_link.sh
+++ apps/ss_rvc_compile_link.sh
@@ -1,5 +1,8 @@
-# Compile source
 echo "Linker with RV32i"
+# incase the file was modified in wondows env 
+dos2unix ss_rvc_compile_link.sh
+
+# Compile source
 riscv32-unknown-elf-gcc  -O3  -march=rv32i -T./ss_rvc_link.common.ld -nostartfiles -D__riscv__ ./$1.S -o $1.elf
 #creates readable elf file
 riscv32-unknown-elf-objdump -gd $1.elf > $1_elf.txt
@@ -9,7 +12,7 @@ riscv32-unknown-elf-objcopy --srec-len 1 --output-target=verilog $1.elf $1_inst_
 if [ ! -d "../verif/tests/$1" ];then
     mkdir ../verif/tests/$1
 fi
-mv $1.S             ../verif/tests/$1
+cp $1.S             ../verif/tests/$1
 mv $1.elf           ../verif/tests/$1
 mv $1_elf.txt       ../verif/tests/$1
 mv $1_inst_mem.sv   ../verif/tests/$1
