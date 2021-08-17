da1671a12c517961618f88dcd9cc6f6df9728276
amichai-bd <amichaibd@gmail.com>
2021-08-17 22:28:16 +0300

    Fix alive test

--- verif/tb/ss_rvc_tb.sv
+++ verif/tb/ss_rvc_tb.sv
@@ -93,11 +93,13 @@ ss_rvc ss_rvc (
     //D_MEM - Load/Store Interface
     .AddressDmQ103H   (AddressDmQ103H),  //output
     .WrDataDmQ103H    (WrDataDmQ103H),   //output
-    .RdEnDmQ103H      (RdEnDmQ103H),     //output
-    .WrEnDmQ103H      (WrEnDmQ103H),     //output
-    .RdDataDmQ104H    (RdDataDmQ104H)    //input
+    .WrEnDmQ103H      (WrEnDmQ103H)      //output
     );
 
+
+//======================
+//  VC of memory - not part of DUT - its part of TB
+//======================
 mem_wrap mem_wrap (
     .QClk             (QClk),            //input
     .RstQnnnH         (RstQnnnH),        //input
@@ -123,9 +125,9 @@ mem_wrap mem_wrap (
     //D_MEM - Load/Store Interface
     .AddressDmQ103H   (AddressDmQ103H),  //input
     .WrDataDmQ103H    (WrDataDmQ103H),   //input
-    .RdEnDmQ103H      (RdEnDmQ103H),     //input
+    .RdEnDmQ103H      ('0),              //input
     .WrEnDmQ103H      (WrEnDmQ103H),     //input
-    .RdDataDmQ104H    (RdDataDmQ104H)    //output
+    .RdDataDmQ104H    ()               //output
 );
 
 //================================================================================
